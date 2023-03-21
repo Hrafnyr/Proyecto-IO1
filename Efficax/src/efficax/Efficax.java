019d51aced64e391eac37490719a3291cdec7bcf
Moises David Maldonado de León <moiseszurdomaldonado@gmail.com>
2023-03-20 23:51:09 -0600

    Inicio de interfaz gráfica

---
+++ Efficax/src/efficax/Efficax.java
@@ -0,0 +1,14 @@
+
+package efficax;
+
+/*
+    Developed by Moises David Maldonad de León 
+ */
+
+public class Efficax {
+
+    public static void main(String[] args) {
+        MainWindow principal = new MainWindow();
+    }
+    
+}
---
+++ Efficax/src/efficax/MyButton.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package efficax;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+public class MyButton extends JButton {
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        setBackground(color);
+    }
+
+    public Color getColorOver() {
+        return colorOver;
+    }
+
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public MyButton() {
+        //  Init Color
+
+        setColor(Color.WHITE);
+        colorOver = new Color(179, 250, 160);
+        borderColor = new Color(30, 136, 56);
+        setContentAreaFilled(false);
+        //  Add event mouse
+    }
+
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color borderColor;
+    private int radius = 0;
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //  Paint Border
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+        //  Border set 2 Pix
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
+        super.paintComponent(grphcs);
+    }
+}
